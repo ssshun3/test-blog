c11f5286da469da35476a605fb0630d2af0238b3
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-02-17 18:25:25 +0900

    Revert "comitto"
    This reverts commit f276baebe64ea786eacfc9975190c0ce72165317.

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,8 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+<<<<<<< HEAD
 AuthenticatorAssertionResponse
+=======
 AudioBufferSourceNode
+>>>>>>> 47538c15eedf1ec459e410a47f87f6f63a11306b
