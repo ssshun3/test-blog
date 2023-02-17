7a7ba39d3a1934ec4d3bfa69f1de46a85a007ca2
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-02-17 18:28:06 +0900

    Revert "aa"
    This reverts commit dbe71cde135f03aef90aeb4587af7b4ff5c1f76b.

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,4 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-<<<<<<< HEAD
-AuthenticatorAssertionResponse
-=======
-AudioBufferSourceNode
->>>>>>> 47538c15eedf1ec459e410a47f87f6f63a11306b
+
